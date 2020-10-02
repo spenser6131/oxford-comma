55dab1381392dacd81fb13d663e3110e34577e3c
spenser6131 <brinkman.spenser@gmail.com>
2020-10-01 18:17:32 -0700

    oxford_comma100120

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >2
+    first = array[0..array.length - 2].join(", ")
+    second = ", and #{array.last}"
+    "#{first}#{second}"
+  end
 end
